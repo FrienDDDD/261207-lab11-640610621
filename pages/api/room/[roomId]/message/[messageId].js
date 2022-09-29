af7a5aee6a40e63bf71f40c845932d1301935465
FrienDDDD <friendjunk@gmail.com>
2022-09-29 19:55:55 +0700

    update

--- pages/api/room/[roomId]/message/[messageId].js
+++ pages/api/room/[roomId]/message/[messageId].js
@@ -9,14 +9,47 @@ export default function roomIdMessageIdRoute(req, res) {
   const roomId = req.query.roomId;
   const messageId = req.query.messageId;
 
-  //check token
+  if (req.method === "DELETE") {
+    //check token
 
-  const rooms = readChatRoomsDB();
+    const user = checkToken(req);
+    const rooms = readChatRoomsDB();
+    if (!user)
+      return res.status(401).json({
+        ok: false,
+        message: "Yon don't permission to access this api",
+      });
 
-  //check if roomId exist
-
-  //check if messageId exist
-
-  //check if token owner is admin, they can delete any message
-  //or if token owner is normal user, they can only delete their own message!
+    const Idx = rooms.findIndex((x) => x.roomId === roomId);
+    //check if roomId exist
+    if (Idx === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    }
+    //check if messageId exist
+    const Idxmessage = rooms[Idx].messages.findIndex(
+      (x) => x.messageId === messageId
+    );
+    if (Idxmessage === -1) {
+      return res.status(404).json({ ok: false, message: "Invalid message id" });
+    }
+    //check if token owner is admin, they can delete any message
+    if (user.isAdmin === true) {
+      rooms[Idx].message.splice(Idxmessage, 1);
+      writeChatRoomsDB(rooms);
+      return res.status(200).json({ ok: true });
+    }
+    //or if token owner is normal user, they can only delete their own message!
+    if (user.isAdmin === false) {
+      if (rooms[Idx].messages[Idxmessage].username === user.username) {
+        rooms[Idx].message.splice(Idxmessage, 1);
+        writeChatRoomsDB(rooms);
+        return res.status(200).json({ ok: true });
+      } else {
+        return res.status(403).json({
+          ok: false,
+          message: "You do not have permission to access this data",
+        });
+      }
+    }
+  }
 }
